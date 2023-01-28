472a04a4bc584748e77a4f0605b6e57b002d0008
ana-jimenez <anajimenez@tamu.edu>
2023-01-28 15:06:43 -0600

    changes

---
+++ data.js
@@ -0,0 +1,79 @@
+var data = [
+{
+    "team" : 1,
+    "strength" : 22,
+    "preferred": [2,4,6,11],
+    "tolerated": [3,8,10],
+    "noWay": [5,7,9]
+},
+{
+    "team" : 2,
+    "strength" : 45,
+    "preferred": [1,3,5],
+    "tolerated": [6,7,11],
+    "noWay": [4,8,9,10]
+},
+{
+    "team" : 3,
+    "strength" : 34,
+    "preferred": [1,2,11],
+    "tolerated": [7],
+    "noWay": [4,5,6,8,9,10]
+},
+{
+    "team" : 4,
+    "strength" : 51,
+    "preferred": [10],
+    "tolerated": [1,3],
+    "noWay": [2,5,6,7,8,9,11]
+},
+{
+    "team" : 5,
+    "strength" : 37,
+    "preferred": [7,10],
+    "tolerated": [1,8],
+    "noWay": [2,3,4,5,9,11]
+},
+{
+    "team" : 6,
+    "strength" : 37,
+    "preferred": [7,10],
+    "tolerated": [1,8],
+    "noWay": [2,3,4,5,9,11]
+},
+{
+    "team" : 7,
+    "strength" : 42,
+    "preferred": [1,2,3,4,5,6],
+    "tolerated": [10,11],
+    "noWay": [8,9]
+},
+{
+    "team" : 8,
+    "strength" : 16,
+    "preferred": [1,10],
+    "tolerated": [2,4,11],
+    "noWay": [3,5,6,7,9]
+},
+{
+    "team" : 9,
+    "strength" : 29,
+    "preferred": [1,5],
+    "tolerated": [2,10],
+    "noWay": [3,4,6,7,8,11]
+},
+{
+    "team" : 10,
+    "strength" : 56,
+    "preferred": [2,6,7,11],
+    "tolerated": [4,5,8],
+    "noWay": [1,3,9]
+},
+{
+    "team" : 11,
+    "strength" : 49,
+    "preferred": [1,4,5],
+    "tolerated": [2,3,6,7,9,10],
+    "noWay": [8]
+}
+]
